798139c828a010834646c800305d02787c4330e0
Marcin Kuthan <marcin.kuthan@allegrogroup.com>
2015-12-18 13:45:06 +0100

    HiveContext used for sparksqlspec

--- src/test/scala/org/mkuthan/spark/SparkSqlSpec.scala
+++ src/test/scala/org/mkuthan/spark/SparkSqlSpec.scala
@@ -1,19 +1,19 @@
 package org.mkuthan.spark
 
-import org.apache.spark.sql._
+import org.apache.spark.sql.hive.HiveContext
 import org.scalatest._
 
 trait SparkSqlSpec extends SparkSpec {
   this: Suite =>
 
-  private var _sqlc: SQLContext = _
+  private var _sqlc: HiveContext = _
 
   def sqlc = _sqlc
 
   override def beforeAll(): Unit = {
     super.beforeAll()
 
-    _sqlc = new SQLContext(sc)
+    _sqlc = new HiveContext(sc)
   }
 
   override def afterAll(): Unit = {
@@ -21,6 +21,5 @@ trait SparkSqlSpec extends SparkSpec {
 
     super.afterAll()
   }
-
 
 }
